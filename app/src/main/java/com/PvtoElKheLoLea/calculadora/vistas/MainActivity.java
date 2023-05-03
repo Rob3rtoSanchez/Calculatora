525c95fc994a2a4603fefd37305870b0b8710d34
Rob3rtoSanchez <robertosaenz4@gmail.com>
2023-05-02 23:53:12 -0600

    Se agrego el proyecto
    Se creo un proyecto en android studio para un a aplicacion movil de calculadora

---
+++ app/src/main/java/com/PvtoElKheLoLea/calculadora/vistas/MainActivity.java
@@ -0,0 +1,16 @@
+package com.PvtoElKheLoLea.calculadora.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.PvtoElKheLoLea.calculadora.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
